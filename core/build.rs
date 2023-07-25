230f197c5c949c5935ee68a25868dac81cfea571
Andrew Cherry <andrew@xyncro.com>
2023-07-25 14:07:21 +0100

    generating expansion tests

--- core/build.rs
+++ core/build.rs
@@ -1,10 +1,11 @@
 use std::{
     env,
-    fs::{
-        self,
-        File,
+    fs::OpenOptions,
+    io::{
+        BufReader,
+        BufWriter,
+        Write,
     },
-    io::BufReader,
     path::Path,
 };
 
@@ -13,41 +14,78 @@ use anyhow::{
     Result,
 };
 use indexmap::IndexMap;
+use quote::{
+    format_ident,
+    quote,
+};
 use serde::Deserialize;
 
 fn main() -> Result<()> {
     let out_dir = env::var_os("OUT_DIR").ok_or_else(|| Error::msg("OUT_DIR not set"))?;
-    let dest_path = Path::new(&out_dir).join("expansion.rs");
-
-    println!("dest_path: {dest_path:?}");
+    let dest_path = Path::new(&out_dir).join("expansion_tests.rs");
 
     let mut output = String::new();
+    let mut index = 0;
 
-    for (name, _set) in load_all()? {
-        output.push_str(&name);
-        output.push_str("\n");
+    for (name, set) in load_all()? {
+        for (template, expansion) in &set.cases {
+            let test_name = format_ident!("expansion_{index}");
+            let vars = &set.variables;
 
-        // for (tpl, exp) in &set.cases {
-        // let vars = &set.variables;
+            match expansion {
+                Expansion::String(expansion) => {
+                    let err = format!("{name}: {template} not eq to {expansion} (vars: {vars:#?}")
+                        .replace("{", "{{")
+                        .replace("}", "}}");
 
-        // match exp {
-        //     Expansion::String(exp) => {
-        //         assert!(exp.eq(tpl), "{name}: {tpl} != {exp}
-        // ({vars:#?})")     }
-        //     Expansion::List(exps) => {
-        //         assert!(exps.contains(tpl), "{name}: {tpl} /∈ {exps:#?}
-        // ({vars:#?})")     }
-        // }
-        // }
+                    output.push_str(
+                        &quote! {
+                            #[test]
+                            fn #test_name() {
+                                assert!(#expansion.eq(#template), #err);
+                            }
+                        }
+                        .to_string(),
+                    );
+                }
+                Expansion::List(expansions) => {
+                    let err = format!("{name}: {template} not in {expansions:#?} (vars: {vars:#?}")
+                        .replace("{", "{{")
+                        .replace("}", "}}");
+
+                    output.push_str(
+                        &quote! {
+                            #[test]
+                            fn #test_name() {
+                                assert!([#(#expansions),*].contains(&#template), #err);
+                            }
+                        }
+                        .to_string(),
+                    );
+                }
+            }
+
+            output.push_str("\n");
+            index += 1;
+        }
     }
 
-    fs::write(&dest_path, &output)?;
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(dest_path)?;
+
+    let mut writer = BufWriter::new(file);
+
+    writer.write(output.as_bytes())?;
 
     Ok(())
 }
 
 // =============================================================================
-// Test Cases
+// Cases
 // =============================================================================
 
 // Types
@@ -86,7 +124,7 @@ fn load_all() -> Result<IndexMap<String, Set>> {
     ["spec-examples", "spec-examples-by-section"]
         .iter()
         .try_fold(IndexMap::new(), |mut all_sets, name| {
-            let path = format!("cases/official/{name}.json");
+            let path = format!("tests/cases/official/{name}.json");
             let new_sets = load(path)?;
 
             all_sets.extend(new_sets.into_iter());
@@ -96,7 +134,7 @@ fn load_all() -> Result<IndexMap<String, Set>> {
 }
 
 fn load(path: impl AsRef<Path>) -> Result<IndexMap<String, Set>> {
-    let file = File::open(path)?;
+    let file = OpenOptions::new().read(true).open(path)?;
     let reader = BufReader::new(file);
     let sets = serde_json::from_reader(reader)?;
 
