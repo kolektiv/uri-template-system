a7f2ba07f4725bcd511d81d445fe614e95f47492
Andrew Cherry <andrew@xyncro.com>
2023-08-02 23:35:52 +0100

    encode tweaking

--- core/src/codec/encode.rs
+++ core/src/codec/encode.rs
@@ -6,118 +6,64 @@ use std::sync::OnceLock;
 
 // Types
 
-// #[derive(Debug)]
 pub struct Encoding {
     pub allow_encoded: bool,
     pub allow: Box<dyn Fn(char) -> bool + Send + Sync>,
 }
 
-enum Buffer {
-    Empty,
-    Percent,
-    HexDigit(char),
-}
-
-impl Buffer {
-    fn extend(&mut self, output: &mut String, encoding: &Encoding, input: char) -> Option<char> {
-        match &self {
-            Self::HexDigit(_) if is_hex_digit(input) => {
-                self.complete(output, input);
-
-                *self = Self::Empty;
-                None
-            }
-            Self::Percent if is_hex_digit(input) => {
-                *self = Self::HexDigit(input);
-                None
-            }
-            _ if is_percent(input) => {
-                self.flush(output, encoding);
-
-                *self = Self::Percent;
-                None
-            }
-            _ => {
-                self.flush(output, encoding);
-
-                *self = Self::Empty;
-                Some(input)
-            }
-        }
-    }
-
-    fn complete(&self, output: &mut String, input: char) {
-        match self {
-            Buffer::HexDigit(hex_digit) => {
-                push_char_utf8('%', output);
-                push_char_utf8(*hex_digit, output);
-                push_char_utf8(input, output);
-            }
-            _ => {}
-        }
-    }
-
-    fn flush(&self, output: &mut String, encoding: &Encoding) {
-        match self {
-            Buffer::HexDigit(hex_digit) => {
-                push_char('%', output, encoding);
-                push_char(*hex_digit, output, encoding);
-            }
-            Buffer::Percent => {
-                push_char('%', output, encoding);
-            }
-            _ => {}
-        }
-    }
-}
-
 // -----------------------------------------------------------------------------
 
-// Functions
+// Encoding
 
 pub fn encode(input: &str, output: &mut String, encoding: &Encoding) {
-    println!("encoding: '{input}'");
-    push_str(input, output, encoding);
+    encode_str(input, output, encoding);
 }
 
-fn push_str(input: &str, output: &mut String, encoding: &Encoding) {
-    let mut buffer = Buffer::Empty;
+fn encode_str(input: &str, output: &mut String, encoding: &Encoding) {
+    let mut state = State::Empty;
 
     for input in input.chars() {
         if encoding.allow_encoded {
-            if let Some(input) = buffer.extend(output, encoding, input) {
-                push_char(input, output, encoding);
+            if let Some(input) = buffer(&mut state, output, encoding, input) {
+                encode_char(input, output, encoding);
             }
         } else {
-            push_char(input, output, encoding);
+            encode_char(input, output, encoding);
         }
     }
 
-    buffer.flush(output, encoding);
+    flush(&state, output, encoding);
 }
 
-fn push_char(input: char, output: &mut String, encoding: &Encoding) {
+fn encode_char(input: char, output: &mut String, encoding: &Encoding) {
     if (&encoding.allow)(input) {
-        push_char_utf8(input, output);
+        encode_char_utf8(input, output);
     } else {
-        push_char_percent(input, output);
+        encode_char_percent(input, output);
     }
 }
 
-fn push_char_utf8(input: char, output: &mut String) {
+fn encode_char_utf8(input: char, output: &mut String) {
     output.push(input);
 }
 
-fn push_char_percent(input: char, output: &mut String) {
+fn encode_char_percent(input: char, output: &mut String) {
     input
         .encode_utf8(&mut [0; 4])
         .as_bytes()
         .iter()
         .for_each(|byte| {
-            get_encoded(*byte)
+            encode_byte_percent(*byte)
                 .iter()
                 .for_each(|char| output.push(*char))
         });
+}
+
+fn encode_byte_percent(input: u8) -> &'static [char; 3] {
+    PERCENT_ENCODED
+        .get_or_init(percent_encoded)
+        .get(usize::from(input))
+        .unwrap()
 }
 
 #[allow(clippy::match_like_matches_macro)]
@@ -140,11 +86,76 @@ fn is_percent(c: char) -> bool {
 
 // -----------------------------------------------------------------------------
 
-// Lookups
+// State
 
-static ENCODED: OnceLock<[[char; 3]; 256]> = OnceLock::new();
+enum State {
+    Empty,
+    Percent,
+    HexDigit(char),
+}
 
-fn init_encoded() -> [[char; 3]; 256] {
+fn buffer(
+    state: &mut State,
+    output: &mut String,
+    encoding: &Encoding,
+    input: char,
+) -> Option<char> {
+    match &state {
+        State::HexDigit(_) if is_hex_digit(input) => {
+            complete(state, output, input);
+            *state = State::Empty;
+            None
+        }
+        State::Percent if is_hex_digit(input) => {
+            *state = State::HexDigit(input);
+            None
+        }
+        _ if is_percent(input) => {
+            flush(state, output, encoding);
+
+            *state = State::Percent;
+            None
+        }
+        _ => {
+            flush(state, output, encoding);
+
+            *state = State::Empty;
+            Some(input)
+        }
+    }
+}
+
+fn complete(state: &State, output: &mut String, input: char) {
+    match state {
+        State::HexDigit(hex_digit) => {
+            encode_char_utf8('%', output);
+            encode_char_utf8(*hex_digit, output);
+            encode_char_utf8(input, output);
+        }
+        _ => {}
+    }
+}
+
+fn flush(state: &State, output: &mut String, encoding: &Encoding) {
+    match state {
+        State::HexDigit(hex_digit) => {
+            encode_char('%', output, encoding);
+            encode_char(*hex_digit, output, encoding);
+        }
+        State::Percent => {
+            encode_char('%', output, encoding);
+        }
+        _ => {}
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+// Percent Encoding
+
+static PERCENT_ENCODED: OnceLock<[[char; 3]; 256]> = OnceLock::new();
+
+fn percent_encoded() -> [[char; 3]; 256] {
     [Vec::from_iter('0'..='9'), Vec::from_iter('A'..='F')]
         .concat()
         .iter()
@@ -158,12 +169,5 @@ fn init_encoded() -> [[char; 3]; 256] {
         .flatten()
         .collect::<Vec<_>>()
         .try_into()
-        .unwrap()
-}
-
-fn get_encoded(input: u8) -> &'static [char; 3] {
-    ENCODED
-        .get_or_init(init_encoded)
-        .get(usize::from(input))
         .unwrap()
 }
