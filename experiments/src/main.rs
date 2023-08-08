3e82deda38a7f1252f06e51aa77f7a0436dd96e3
Andrew Cherry <andrew@xyncro.com>
2023-08-08 10:49:58 +0100

    tiny tweak just to trigger percent encoding

--- experiments/src/main.rs
+++ experiments/src/main.rs
@@ -5,12 +5,12 @@ use uri_template_system_core::{
 };
 
 fn main() {
-    let template = URITemplate::parse("/literal/{simple}/{/expanded*}").unwrap();
+    let template = URITemplate::parse("/literal/{simple}{/expanded*}").unwrap();
     let values = Values::from_iter([
         ("simple".into(), Value::Item("hello".into())),
         (
             "expanded".into(),
-            Value::List(vec!["world1".into(), "world2".into(), "world3".into()]),
+            Value::List(vec!["world1".into(), "world 2".into(), "world3".into()]),
         ),
     ]);
 
