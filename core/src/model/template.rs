af72596521f5b454e7c5671d5a58368635fdef03
Andrew Cherry <andrew@xyncro.com>
2023-08-19 00:20:26 +0100

    some documentation at type level

--- core/src/model/template.rs
+++ core/src/model/template.rs
@@ -29,17 +29,41 @@ use crate::{
 
 // Types
 
+/// The `Template` type is the basis for most simple tasks. Parsing and
+/// expansion are both template functions.
 #[derive(Debug, Eq, PartialEq)]
 pub struct Template<'t> {
     components: Vec<Component<'t>>,
 }
 
 impl<'t> Template<'t> {
+    /// Creates a new `Expansion` using the given `Values`, which may then be
+    /// rendered using the `to_string` function provided by the `Display` trait.
+    ///
+    /// ```
+    /// # use uri_template_system_core::{ Template, Values, Value };
+    /// #
+    /// let template = Template::parse("hello/{name}!").unwrap();
+    /// let values = Values::default().add("name", Value::item("world"));
+    ///
+    /// assert_eq!("hello/world!", template.expand(&values).to_string());
     #[must_use]
     pub const fn expand<'e>(&'e self, values: &'e Values) -> Expansion<'e, 't> {
         Expansion::new(self, values)
     }
 
+    /// Parses a string representing a potential template, and returns a new
+    /// template instance if valid. See https://datatracker.ietf.org/doc/html/rfc6570
+    /// for the grammar of a valid URI Template. `uri-template-system` supports
+    /// all operators and modifiers up-to and including Level 4.
+    ///
+    /// ```
+    /// # use uri_template_system_core::Template;
+    /// #
+    /// let template = Template::parse("my/valid/{template}");
+    ///
+    /// assert!(template.is_ok());
+    /// ```
     pub fn parse(raw: &'t str) -> Result<Self, ParseError> {
         Self::try_parse(raw, 0).map(|(_, template)| template)
     }
