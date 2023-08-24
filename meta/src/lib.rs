baf7f504d8dd2fe185bb56e3320cc562194c66d5
Andrew Cherry <andrew@xyncro.com>
2023-08-24 11:52:54 +0100

    msrv and lint settings

--- meta/src/lib.rs
+++ meta/src/lib.rs
@@ -1,3 +1,11 @@
+#![deny(clippy::all)]
+#![deny(clippy::complexity)]
+#![deny(clippy::nursery)]
+#![deny(clippy::pedantic)]
+#![deny(clippy::perf)]
+#![deny(clippy::style)]
+#![deny(clippy::suspicious)]
+#![deny(missing_docs)]
 #![doc = include_str!("../README.md")]
 
 pub use uri_template_system_core::*;
--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,2 +1,11 @@
+#![deny(clippy::all)]
+#![deny(clippy::complexity)]
+#![deny(clippy::nursery)]
+#![deny(clippy::pedantic)]
+#![deny(clippy::perf)]
+#![deny(clippy::style)]
+#![deny(clippy::suspicious)]
+#![doc = include_str!("../README.md")]
+
 pub mod fixtures;
 pub mod harnesses;
